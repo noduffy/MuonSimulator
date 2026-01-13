7f8820748d7823321e9dc0f2c6ba6c781caac741
noduffy.1267 <noduffy.1267@gmail.com>
2026-01-13 15:30:52 +0900

    pbブロックを調整しやすくした

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -1,15 +1,14 @@
-#include "DetectorConstruction.hh"
-
-#include "G4Box.hh"
-#include "G4LogicalVolume.hh"
-#include "G4NistManager.hh"
-#include "G4PVPlacement.hh"
-#include "G4SystemOfUnits.hh"
-#include "G4ThreeVector.hh"
-#include "G4VisAttributes.hh"
-
 #ifndef WITH_PB
 #define WITH_PB 1
+#endif
+
+// PB_COUNT: Pbブロックの個数（1 または 2 のみ許容）
+#ifndef PB_COUNT
+#define PB_COUNT 2
+#endif
+
+#if (PB_COUNT != 1) && (PB_COUNT != 2)
+#error "PB_COUNT must be 1 or 2."
 #endif
 
 G4VPhysicalVolume* DetectorConstruction::Construct() {
@@ -17,44 +16,39 @@ G4VPhysicalVolume* DetectorConstruction::Construct() {
   auto air  = nist->FindOrBuildMaterial("G4_AIR");
   auto sci  = nist->FindOrBuildMaterial("G4_PLASTIC_SC_VINYLTOLUENE");
 
-  // --- 変更点1: Worldを大きくする ---
-  // 検出器を大きくするため、世界そのものも広げないとエラーになります。
-  // 15cm -> 30cm (60cm立方) に拡大
   auto worldBox = new G4Box("World", 30*cm, 30*cm, 30*cm);
   auto worldLV  = new G4LogicalVolume(worldBox, air, "WorldLV");
   auto worldPV  = new G4PVPlacement(nullptr, {}, worldLV, "WorldPV", nullptr, false, 0);
 
-  // --- 変更点2: 検出器を巨大化 & 配置調整 ---
-  // 10cm -> 15cm (30cm四方) に拡大。これで斜めのミューオンを拾います。
   auto plateBox = new G4Box("Plate", 15*cm, 15*cm, 0.5*mm);
   auto plateLV  = new G4LogicalVolume(plateBox, sci, "PlateLV");
-  
-  // 位置を ±3cm -> ±8cm に変更
-  // (検出器が大きくなったので少し離しても角度は稼げますし、回転実験もしやすくなります)
-  new G4PVPlacement(nullptr, G4ThreeVector(0,0, +8*cm), plateLV, "TopPlate",    worldLV, false, 0);
-  new G4PVPlacement(nullptr, G4ThreeVector(0,0, -8*cm), plateLV, "BottomPlate", worldLV, false, 1);
 
+  new G4PVPlacement(nullptr, G4ThreeVector(0,0, +8*cm), plateLV, "TopPlate",     worldLV, false, 0);
+  new G4PVPlacement(nullptr, G4ThreeVector(0,0, -8*cm), plateLV, "BottomPlate",  worldLV, false, 1);
 
-  #if WITH_PB
-    // 鉛ブロック（中心 20mm 立方）
-    // 配置は前回の実験（2つ置き）のままにしてあります
-    auto pb   = nist->FindOrBuildMaterial("G4_Pb");
-    auto pbBox = new G4Box("PbBlock", 10*mm, 10*mm, 10*mm);
-    auto pbLV  = new G4LogicalVolume(pbBox, pb, "PbLV");
-    
-    // 中央に1つ配置
-    new G4PVPlacement(nullptr, G4ThreeVector(0*mm,0,0), pbLV, "PbBlockPV", worldLV, false, 0);
+#if WITH_PB
+  // 鉛ブロック（中心 20mm 立方）
+  auto pb    = nist->FindOrBuildMaterial("G4_Pb");
+  auto pbBox = new G4Box("PbBlock", 10*mm, 10*mm, 10*mm);
+  auto pbLV  = new G4LogicalVolume(pbBox, pb, "PbLV");
 
-    // 左右に配置 (-40mm, +40mm)
-    // new G4PVPlacement(nullptr, G4ThreeVector(-40*mm,0,0), pbLV, "PbBlockPV", worldLV, false, 0);
-    // new G4PVPlacement(nullptr, G4ThreeVector(+40*mm,0,0), pbLV, "PbBlockPV", worldLV, false, 1);
+  // 可視化（LVに対して1回でOK）
+  auto visPb = new G4VisAttributes(G4Colour(0.2,0.2,0.2,0.8));
+  visPb->SetForceSolid(true);
+  pbLV->SetVisAttributes(visPb);
 
-    auto visPb = new G4VisAttributes(G4Colour(0.2,0.2,0.2,0.8));
-    visPb->SetForceSolid(true);
-    pbLV->SetVisAttributes(visPb);
-  #endif
+#if PB_COUNT == 1
+  // 1個: 中央に配置
+  new G4PVPlacement(nullptr, G4ThreeVector(0*mm, 0, 0), pbLV, "PbBlockPV_C", worldLV, false, 0);
 
-  // 可視化属性
+#elif PB_COUNT == 2
+  // 2個: 左右に配置 (-40mm, +40mm)
+  new G4PVPlacement(nullptr, G4ThreeVector(-40*mm, 0, 0), pbLV, "PbBlockPV_L", worldLV, false, 0);
+  new G4PVPlacement(nullptr, G4ThreeVector(+40*mm, 0, 0), pbLV, "PbBlockPV_R", worldLV, false, 1);
+#endif
+
+#endif // WITH_PB
+
   worldLV->SetVisAttributes(G4VisAttributes::GetInvisible());
   auto visPlate = new G4VisAttributes(G4Colour(0.2,0.8,0.2,0.4));
   plateLV->SetVisAttributes(visPlate);
